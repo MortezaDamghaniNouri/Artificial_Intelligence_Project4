323a4842911f61b9dceec3cdd511f5cd48d3d83e
Morteza-Damghani <mortezadamghani77@gmail.com>
2021-07-13 11:23:05 -0700

    Some progress

--- Main.py
+++ Main.py
@@ -11,9 +11,16 @@ def file_reader(input_file_name, input_list):
 def bigram_dictionary_generator(sentences_list, words_dictionary):
     for sentence in sentences_list:
         words_list = sentence.split()
-        for word in words_list:
-            if word == "." or word == "," or word == "  " or word == ";" or word == "\"" or word == "\'" or word == "*" or word == "(" or word == ")":
-                words_list.remove(word)
+        redundant_words_index = []
+        i = 0
+        while i < len(words_list):
+            if words_list[i] == "." or words_list[i] == "," or words_list[i] == "  " or words_list[i] == ";" or words_list[i] == "\"" or words_list[i] == "\'" or words_list[i] == "*" or words_list[i] == "(" or words_list[i] == ")" or words_list[i] == "--" or words_list[i] == "-"\
+                    or words_list[i] == "?" or words_list[i] == "!" or words_list[i] == "&" or words_list[i] == ":" or words_list[i] == "." or words_list[i] == "و":
+                redundant_words_index.append(words_list[i])
+
+            i += 1
+        for i in redundant_words_index:
+            words_list.remove(i)
 
         # Adding each useful word to the dictionary
         for word in words_list:
@@ -54,10 +61,48 @@ positive_words_dictionary = {}
 negative_words_dictionary = {}
 bigram_dictionary_generator(positive_comments_list, positive_words_dictionary)
 bigram_dictionary_generator(negative_comments_list, negative_words_dictionary)
+# Sorting two generated dictionaries
+positive_words_list = sorted(positive_words_dictionary.items(), key=lambda x: x[1], reverse=True)
+negative_words_list = sorted(negative_words_dictionary.items(), key=lambda x: x[1], reverse=True)
+# Removing top ten words
+i = 0
+while i < 9:
+    positive_words_list.remove(positive_words_list[i])
+    i += 1
+i = 0
+while i < 9:
+    negative_words_list.remove(negative_words_list[i])
+    i += 1
+
+
+positive_words_dictionary = {}
+for i in positive_words_list:
+    positive_words_dictionary[i[0]] = [i[1]]
+negative_words_dictionary = {}
+for i in negative_words_list:
+    negative_words_dictionary[i[0]] = [i[1]]
+
+redundant_words = []
+for i in positive_words_dictionary:
+    if positive_words_dictionary[i][0] <= 2:
+        redundant_words.append(i)
+for i in redundant_words:
+    positive_words_dictionary.pop(i)
+
+redundant_words = []
+for i in negative_words_dictionary:
+    if negative_words_dictionary[i][0] <= 2:
+        redundant_words.append(i)
+for i in redundant_words:
+    negative_words_dictionary.pop(i)
 
 
 
-
+# Dictionary printer
+# output_file = open("output.txt", "wt")
+# for i in positive_words_dictionary:
+#     output_file.write(i + ": " + str(positive_words_dictionary[i][0]) + "\n")
+# output_file.close()
 
 
 
