56de0f1a257a03af038893b3f5d300b8d5e7705f
Morteza-Damghani <mortezadamghani77@gmail.com>
2021-07-14 00:13:54 -0700

    Probabilities calculated.

--- Main.py
+++ Main.py
@@ -65,22 +65,63 @@ bigram_dictionary_generator(negative_comments_list, negative_words_dictionary)
 positive_words_list = sorted(positive_words_dictionary.items(), key=lambda x: x[1], reverse=True)
 negative_words_list = sorted(negative_words_dictionary.items(), key=lambda x: x[1], reverse=True)
 # Removing top ten words
+redundant_words_list = []
 i = 0
 while i < 9:
-    positive_words_list.remove(positive_words_list[i])
+    redundant_words_list.append(positive_words_list[0][0])
+    positive_words_list.remove(positive_words_list[0])
     i += 1
-i = 0
-while i < 9:
-    negative_words_list.remove(negative_words_list[i])
-    i += 1
-
 
 positive_words_dictionary = {}
 for i in positive_words_list:
-    positive_words_dictionary[i[0]] = [i[1]]
+    positive_words_dictionary[i[0]] = i[1]
+
+redundant_pairs_list = []
+for i in redundant_words_list:
+    redundant_word = i + " "
+    for j in positive_words_dictionary:
+        if j.find(redundant_word) != -1:
+            redundant_pairs_list.append(j)
+
+for i in redundant_pairs_list:
+    positive_words_dictionary.pop(i)
+
+
+
+redundant_words_list = []
+i = 0
+while i < 9:
+    redundant_words_list.append(negative_words_list[0][0])
+    negative_words_list.remove(negative_words_list[0])
+    i += 1
+
 negative_words_dictionary = {}
 for i in negative_words_list:
-    negative_words_dictionary[i[0]] = [i[1]]
+    negative_words_dictionary[i[0]] = i[1]
+
+redundant_pairs_list = []
+for i in redundant_words_list:
+    redundant_word = i + " "
+    for j in negative_words_dictionary:
+        if j.find(redundant_word) != -1:
+            redundant_pairs_list.append(j)
+
+for i in redundant_pairs_list:
+    negative_words_dictionary.pop(i)
+
+
+
+positive_words_list = sorted(positive_words_dictionary.items(), key=lambda x: x[1], reverse=True)
+negative_words_list = sorted(negative_words_dictionary.items(), key=lambda x: x[1], reverse=True)
+
+positive_words_dictionary = {}
+for i in positive_words_list:
+    positive_words_dictionary[i[0]] = []
+    positive_words_dictionary[i[0]].append(i[1])
+negative_words_dictionary = {}
+for i in negative_words_list:
+    negative_words_dictionary[i[0]] = []
+    negative_words_dictionary[i[0]].append(i[1])
 
 redundant_words = []
 for i in positive_words_dictionary:
@@ -98,12 +139,52 @@ for i in redundant_words:
 
 
 
-# Dictionary printer
-# output_file = open("output.txt", "wt")
-# for i in positive_words_dictionary:
-#     output_file.write(i + ": " + str(positive_words_dictionary[i][0]) + "\n")
-# output_file.close()
+m_variable_positive_dictionary = 0  # The number of repetition of all words in positive dictionary is stored here
+for i in positive_words_dictionary:
+    m_variable_positive_dictionary += positive_words_dictionary[i][0]
 
+m_variable_negative_dictionary = 0  # The number of repetition of all words in negative dictionary is stored here
+for i in negative_words_dictionary:
+    m_variable_negative_dictionary += negative_words_dictionary[i][0]
+
+
+# Finding the probability of each word in each dictionary
+positive_words_keys = list(positive_words_dictionary)
+i = 0
+while i < len(positive_words_keys):
+    if positive_words_keys[i].find(" ") != -1:
+        words = positive_words_keys[i]
+        words_list = words.split()
+        first_word = words_list[0]
+        positive_words_dictionary[positive_words_keys[i]].append(positive_words_dictionary[positive_words_keys[i]][0] / positive_words_dictionary[first_word][0])
+    else:
+        positive_words_dictionary[positive_words_keys[i]].append(positive_words_dictionary[positive_words_keys[i]][0] / m_variable_positive_dictionary)
+    i += 1
+
+
+negative_words_keys = list(negative_words_dictionary)
+i = 0
+while i < len(negative_words_keys):
+    if negative_words_keys[i].find(" ") != -1:
+        words = negative_words_keys[i]
+        words_list = words.split()
+        first_word = words_list[0]
+        negative_words_dictionary[negative_words_keys[i]].append(negative_words_dictionary[negative_words_keys[i]][0] / negative_words_dictionary[first_word][0])
+    else:
+        negative_words_dictionary[negative_words_keys[i]].append(negative_words_dictionary[negative_words_keys[i]][0] / m_variable_negative_dictionary)
+    i += 1
+
+
+
+# Dictionary printer
+output_file = open("positive_output.txt", "wt")
+for i in positive_words_dictionary:
+    output_file.write(i + ": " + str(positive_words_dictionary[i]) + "\n")
+output_file.close()
+output_file = open("negative_output.txt", "wt")
+for i in negative_words_dictionary:
+    output_file.write(i + ": " + str(negative_words_dictionary[i]) + "\n")
+output_file.close()
 
 
 
